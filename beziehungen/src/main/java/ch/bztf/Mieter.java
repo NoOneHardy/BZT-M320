e9ef848ac7224b468cc5f72e9ee0e7b7ea351673
NoOneHardy <silas.hardegger@outlook.com>
2023-11-28 13:29:04 +0100

    Update Mieter.java

--- beziehungen/src/main/java/ch/bztf/Mieter.java
+++ beziehungen/src/main/java/ch/bztf/Mieter.java
@@ -29,5 +29,29 @@ public class Mieter {
     }
 
     public void wohnungMieten(Wohnung wohnung) {
+        if (!wohnung.isFrei()) {
+            System.out.println("Diese Wohnung wird bereits an " + wohnung.getMieter().getName() + " vermietet.");
+            return;
+        }
+
+        wohnungen.add(wohnung);
+        anzahlWohnungen++;
+        wohnung.setMieter(this);
+    }
+
+    public void mietvertragKuenden(Wohnung wohnung) {
+        if (wohnung.isFrei()) {
+            System.out.println("Diese Wohnung wird nicht vermietet.");
+            return;
+        }
+
+        if (wohnung.getMieter() != this || !wohnungen.contains(wohnung)) {
+            System.out.println("Diese Wohnung wird nicht an diesen Mieter vermietet.");
+            return;
+        }
+
+        wohnungen.remove(wohnung);
+        anzahlWohnungen--;
+        wohnung.setMieter(null);
     }
 }
